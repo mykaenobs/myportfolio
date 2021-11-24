bc8ff8a13cd0af0fbac7f8a0bde5739a42f1aaa0
Myka Enobs <mykaenobs@gmail.com>
2021-11-24 23:56:59 +0100

    Remove the hire button

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,6 @@
 import Navbar from "components/navbar";
 import Title from "components/title";
 import Footer from "components/footer";
-import Hire from "components/hire";
 import {BtnContact} from "components/buttons";
 import ContactForm from "components/contactForm";
 import axios from 'axios';
@@ -11,9 +10,8 @@ const Contact = ({ services }) => {
     <>
       <div className={"bg-purple-900 pl-8 pr-8"}>
         <Navbar />
-        {/*<Hire />*/}
       </div>
-      <Title subTitle="Contact Us" title="Get in touch with us" />
+      <Title subTitle="Contact Us" title="Get in touch with us!" />
       <BtnContact />
       <ContactForm services={services} />
       <Footer />
