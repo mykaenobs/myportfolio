6a71a02ce4da9daba3112d5ee071a19e3a09c55e
Myka Enobs <mykaenobs@gmail.com>
2021-12-03 16:54:20 +0100

    add the call button

--- components/buttons.js
+++ components/buttons.js
@@ -18,7 +18,7 @@ const BtnContact = () => {
   return (
     <div className="mt-20 flex gap-8 items-center justify-center pl-8 pr-8">
       <hr className="border-grey-100 w-full" />
-        <Link href="callto:+2348023457784"><a className="p-3 pl-5 pr-5 bg-red-200 text-white rounded-xl text-sm whitespace-nowrap" title={+2348023457784}>Call Us Directly</a></Link>
+        <Link href="tel:+2348023457784"><a className="p-3 pl-5 pr-5 bg-red-200 text-white rounded-xl text-sm whitespace-nowrap" title={+2348023457784}>Call Us Directly</a></Link>
         <Link href="mailto: mykaenobs@gmail.com"><a className="p-3 pl-5 pr-5 border-2 border-red-100 text-red-100 rounded-xl text-sm whitespace-nowrap" title={'mykaenobs@gmail.com'}>Email Us Directly</a></Link>
       <hr className="border-grey-100 w-full" />
     </div>
--- components/hire.js
+++ components/hire.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Hire = () => {
   return (
     <div className="rotate-12 pointer-events-auto">
-      <Link href="callto:+2348075961726">
+      <Link href="tel:+2348075961726">
         <a className="transform -rotate-45 w-16 h-16 -mt-8 -ml-8 bg-white border-2 border-red-100 text-red-100 text-center flex items-center justify-center rounded-full fixed left-2/4 top-2/4 font-jet text-xs md:text-sm font-medium uppercase shadow-md z-40 md:h-20 md:w-20 md:-ml-10 md:-mt-10">
           Hire <br /> Us
         </a>
