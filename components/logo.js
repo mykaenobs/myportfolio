13296bb0d234a0640cc5f022ccd2ebc50b622466
Myka Enobs <mykaenobs@gmail.com>
2022-01-07 12:10:09 +0100

    Working on the logic for the contact us page

--- components/logo.js
+++ components/logo.js
@@ -1,15 +1,21 @@
 import Link from 'next/link';
 
-const Logo = ({pos}) => {
+const Logo = ({ pos }) => {
   switch (pos) {
     case 'menu':
-      return <Link href="/"><a href="" className="visible text-red-100 text-2xl font-bold md:invisible">ae.</a></Link>
+      return <Link href="/">
+        <a href="" className="visible text-red-100 text-xl font-bold md:invisible">Aenobs.</a>
+      </Link>;
     case 'foot':
-      return <Link href="/"><a href="" className="text-red-100 text-2xl font-bold">ae.</a></Link>
+      return <Link href="/">
+        <a href="" className="text-red-100 text-xl font-bold">Aenobs.</a>
+      </Link>;
     default:
       return (
         <h1>
-          <Link href="/"><a href="" className="text-white text-2xl font-bold">ae.</a></Link>
+          <Link href="/">
+            <a href="" className="text-white text-xl font-bold">Aenobs.</a>
+          </Link>
         </h1>
       );
   }
